9f410f57712a4157323c9077d608cae76141d271
eatradish <sakiiily@aosc.io>
2022-12-12 16:59:15 +0800

    doc: improve doc

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,27 @@ pub enum Item {
 
 type NomParseItem<'a> = Vec<(&'a [u8], (&'a [u8], Vec<u8>))>;
 
+/// Parse a single package:
+///
+/// ```rust
+/// use std::process::Command;
+/// use eight_deep_parser::{parse_multi, parse_one, Item};
+/// 
+/// let command = Command::new("dpkg")
+///     .arg("-s")
+///     .arg("plasma-workspace")
+///     .output()
+///     .unwrap();
+/// 
+/// let stdout = command.stdout;
+///
+/// let r = parse_one(std::str::from_utf8(&stdout).unwrap()).unwrap();
+///
+/// assert_eq!(
+///     r.get("Package").unwrap(),
+///     &Item::OneLine("plasma-workspace".to_string())
+/// );
+///```
 pub fn parse_one<'a, E: ParseError<&'a [u8]>>(
     s: &'a str,
 ) -> Result<HashMap<String, Item>, EightDParseError<E>>
@@ -33,6 +54,29 @@ where
     Ok(result)
 }
 
+/// Parse multi package:
+/// (e.g: /var/lib/dpkg/status)
+///
+/// ```rust
+/// use std::{fs, io::Read, process::Command};
+/// use eight_deep_parser::{parse_multi, Item};
+/// 
+/// let dir = fs::read_dir("/var/lib/apt/lists").unwrap();
+///
+/// for i in dir.flatten() {
+///     if !i.file_name().to_str().unwrap().ends_with("_Packages") {
+///         continue;
+///     }
+///
+///     let mut f = std::fs::File::open(i.path()).unwrap();
+///     let mut buf = Vec::new();
+///     f.read_to_end(&mut buf).unwrap();
+///
+///     let r = parse_multi(std::str::from_utf8(&buf).unwrap());
+///
+///     assert!(r.is_ok())
+/// }
+/// ```
 pub fn parse_multi<'a, E: ParseError<&'a [u8]>>(
     s: &'a str,
 ) -> Result<Vec<HashMap<String, Item>>, EightDParseError<E>>
